f000402ccd7dbf10e7d2d84d613a1a11c87a4a18
Ajesh Mishra <ajesh.sit@gmail.com>
2022-08-05 20:41:06 +0530

    adds keyboard row solution

---
+++ keyboard_row/src/main.rs
@@ -0,0 +1,93 @@
+struct Solution {
+    q_row: String,
+    h_row: String,
+}
+
+impl Solution {
+    pub fn new() -> Self {
+        Solution {
+            q_row: "qwertyuiopQWERTYUIOP".to_string(),
+            h_row: "asdfghjklASDFGHJKL".to_string(),
+        }
+    }
+
+    pub fn find_words(self, words: Vec<String>) -> Vec<String> {
+        let mut output: Vec<String> = Vec::new();
+
+        for word in words {
+            let mut count: i32 = 0;
+
+            for w in word.chars() {
+                if self.q_row.contains(w) {
+                    count += 1;
+                } else if self.h_row.contains(w) {
+                    count += 5;
+                }
+            }
+
+            match count.abs() {
+                0 => output.push(word),
+                len => {
+                    if len == word.len() as i32 || len == 5 * word.len() as i32 {
+                        output.push(word)
+                    } 
+                }
+            }
+        }
+
+        output
+    }
+}
+
+fn main() {
+    let words = vec![
+        String::from("Hello"),
+        String::from("Alaska"),
+        String::from("Dad"),
+        String::from("Peace"),
+    ];
+
+    let s = Solution::new();
+    println!("{:?}", s.find_words(words));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::iter::zip;
+
+    fn compare_vector(a: &Vec<String>, e: &Vec<String>) -> bool {
+        if a.len() != e.len() {
+            return false;
+        }
+        for (aw, ew) in zip(a, e) {
+            if aw != ew {
+                return false;
+            }
+        }
+        true
+    }
+
+    #[test]
+    fn tc_1() {
+        let words = vec!["omk".to_string()];
+        let s = Solution::new();
+        let answer = s.find_words(words);
+        println!("{:?}", answer);
+        assert!(compare_vector(&answer, &vec![]));
+    }
+
+    #[test]
+    fn tc_2() {
+        let words = vec![
+            "adsdf".to_string(),
+            "sfd".to_string()
+        ];
+        let s = Solution::new();
+        let answer = s.find_words(words);
+        assert!(compare_vector(&answer, &vec![
+            "adsdf".to_string(),
+            "sfd".to_string()
+        ]));
+    }
+}
