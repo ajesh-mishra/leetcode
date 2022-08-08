0f11338439438aa5b0d88bad76dd0056e53e8fb6
Ajesh Mishra <ajesh.sit@gmail.com>
2022-08-08 19:47:06 +0530

    adds relative rank solution

---
+++ relative_ranks/src/main.rs
@@ -0,0 +1,91 @@
+use std::collections::HashMap;
+
+struct Solution {}
+
+impl Solution {
+    pub fn find_relative_ranks(score: Vec<i32>) -> Vec<String> {
+        let len = score.len();
+        let mut rank = score.clone();
+        let mut result = Vec::new();
+        
+        let podium_str: HashMap<usize, &str> =
+            HashMap::from([(0, "Gold Medal"), (1, "Silver Medal"), (2, "Bronze Medal")]);
+
+        for i in 0..len {
+            for j in 0..(len - i - 1) {
+                if rank[j] < rank[j + 1] {
+                    let temp = rank[j + 1];
+                    rank[j + 1] = rank[j];
+                    rank[j] = temp;
+                }
+            }
+        }
+
+        for s in score {
+            let pos = rank.iter().position(|&x| x == s).unwrap();
+
+            if pos < 3 {
+                if let Some(x) = podium_str.get(&pos) {
+                    result.push(x.to_string());
+                } 
+            } else {
+                result.push(format!("{}", pos + 1));
+            }
+        }
+
+        result
+    }
+}
+
+fn main() {
+    let score = vec![5, 4, 3, 2, 1];
+    println!("{:?}", Solution::find_relative_ranks(score));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::iter::zip;
+
+    fn compare_vector(output: &Vec<String>, expected: &Vec<String>) -> bool {
+        if output.len() != expected.len() {
+            return false;
+        }
+        for (o, e) in zip(output, expected) {
+            if o != e {
+                return false;
+            }
+        }
+        true
+    }
+
+    #[test]
+    fn tc_1() {
+        let score = vec![5, 4, 3, 2, 1];
+        let output = Solution::find_relative_ranks(score);
+        let expected = vec![
+            "Gold Medal".to_string(),
+            "Silver Medal".to_string(),
+            "Bronze Medal".to_string(),
+            "4".to_string(),
+            "5".to_string(),
+        ];
+        println!("{:?}", output);
+        assert!(compare_vector(&output, &expected));
+    }
+
+    #[test]
+    fn tc_2() {
+        let score = vec![10,3,8,9,4];
+        let output = Solution::find_relative_ranks(score);
+        let expected = vec![
+            "Gold Medal".to_string(),
+            "5".to_string(),
+            "Bronze Medal".to_string(),
+            "Silver Medal".to_string(),
+            "4".to_string(),
+        ];
+        println!("{:?}", output);
+        assert!(compare_vector(&output, &expected));
+    }
+}
